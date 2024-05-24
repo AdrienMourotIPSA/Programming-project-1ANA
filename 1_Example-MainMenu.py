48cd9715d992e2df60bd53836f7bc1d5d2a2b72d
AdrienMourotIPSA <adrien.mourot@ipsa.fr>
2024-05-24 23:19:27 +0200

    added some commentaries

--- 1_Example-MainMenu.py
+++ 1_Example-MainMenu.py
@@ -6,11 +6,11 @@ import pygame_menu
 from pygame_menu import themes
 from typing import Tuple, Any
 
-pygame.init()
+pygame.init() #here, we define the bases characteristics of the window to launch
 screen = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption('IPSA GAME PLATFORM')
 
-def signup_button():
+def signup_button(): #here is some functions, this one define the actions when we click on the button to sign up
     c1 = courriel1.get_value()
     m1 = mdp1.get_value()
     file = open('File_username.csv', 'r')
@@ -18,8 +18,8 @@ def signup_button():
     flag=0
     for line in lines:
         line = line.strip()
-        l = line.split(',')
-        if l[0] == c1:
+        l = line.split(',') #the following lines search in the database if the email already exist 
+        if l[0] == c1: #if yes, it returns to the user that it already exist
             colour1 = (200, 0, 0)
             fontObj = pygame.font.Font('freesansbold.ttf', 15)  
             text = fontObj.render("Already signed up email!", True, colour1)
@@ -27,7 +27,7 @@ def signup_button():
             pygame.display.update()
             pygame.time.delay(1000)
             flag=1
-    if flag==0:
+    if flag==0: #if not, it returns that the user is now registered
         if len(m1)>6:
             file=open("File_username.csv", "a")
             file.write(f"\n{c1},{m1}")
@@ -38,7 +38,7 @@ def signup_button():
             pygame.display.update()
             pygame.time.delay(1000)
             file.close()
-        elif len(m1)<6:
+        elif len(m1)<6: #these few lines check if the password is much long as necessary
             colour1 = (200, 0, 0)
             fontObj = pygame.font.Font('freesansbold.ttf', 15)  
             text = fontObj.render("The password is too short (min 6 characters)", True, colour1)
@@ -47,7 +47,7 @@ def signup_button():
             pygame.time.delay(1000)
             
 
-def login_button():
+def login_button(): #this function check in the data base if the email associated to the password are in the database
     c2 = courriel2.get_value()
     m2 = mdp2.get_value()
     file = open('File_username.csv', 'r')
@@ -57,12 +57,12 @@ def login_button():
         line = line.strip()
         l = line.split(',')
         flag=0
-        if l[0] == c2 and l[1] == m2:
+        if l[0] == c2 and l[1] == m2: #if yes, it imports another python file which is another window to open (work in progress...)
             import Afterloginsettings
             flag=1
             return flag
             
-    if flag==0 and (l[0] != c2 or l[1] != m2):
+    if flag==0 and (l[0] != c2 or l[1] != m2): #if not, it returns to the user that there is an incorrect parameter
         colour1 = (200, 0, 0)
         fontObj = pygame.font.Font('freesansbold.ttf', 15)  
         text = fontObj.render("Username or password is not correct!", True, colour1)
@@ -82,15 +82,11 @@ def set_difficulty(set_difficulty: Tuple, value: Any) -> None:
  
 def start_the_game():
     print(difficulty)
-
-
-def login_function():
-    print(courriel1.get_value())
          
 level = pygame_menu.Menu('Select a Difficulty', 400, 400, theme=themes.THEME_SOLARIZED)
 level.add.selector('levels :', [('level1', 1), ('level2', 2)], onchange=set_difficulty)
 
-mainmenu = pygame_menu.Menu('Welcome to main menu!', 1280, 720, theme=themes.THEME_SOLARIZED)
+mainmenu = pygame_menu.Menu('Welcome to main menu!', 1280, 720, theme=themes.THEME_SOLARIZED) #theses few lines set up the different things to show on the main menu
 courriel1 = mainmenu.add.text_input('Email: ', default='Email', maxchar=20)
 mdp1 = mainmenu.add.text_input('Password: ', default='Password', maxchar=20)
 mainmenu.add.button('Sign up', signup_button)
@@ -99,8 +95,6 @@ courriel2 = mainmenu.add.text_input('Email: ', default='Email', maxchar=20)
 mdp2 = mainmenu.add.text_input('Password: ',default='Password', maxchar=20)
 mainmenu.add.button('Log in', login_button)
 mainmenu.add.text_input('',maxchar=0)
-mainmenu.add.button('Quit', pygame_menu.events.EXIT)
+mainmenu.add.button('Quit', pygame_menu.events.EXIT) # type: ignore
 
 mainmenu.mainloop(screen)
-
-pygame.display.get_caption() 
